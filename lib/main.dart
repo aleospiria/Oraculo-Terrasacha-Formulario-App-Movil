4104717aab6e7126946d9d72b45bcdff1006d654
Aleospiria <Alejoospi779@gmail.com>
2026-01-12 15:58:20 -0500

    GraphQl implementado en la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'Screens/CapturaDatosScreen.dart';
 import 'Screens/ParcelasMenuScreen.dart';
@@ -9,29 +10,56 @@ import 'Screens/RevisionScreen.dart';
 import 'Screens/RegistrosGuardadosScreen.dart';
 import 'theme.dart';
 
-void main() {
-  runApp(const CapturadorApp());
+//Configurar con el endpoint y api key real
+const String graphQLEndpoint = 'https://TU-ENDPOINT.appsync-api.TU-REGION.amazonaws.com/graphql';
+const String apiKey = 'TU_API_KEY';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    graphQLEndpoint,
+    defaultHeaders: {
+      'x-api-key': apiKey,
+    },
+  );
+
+  final ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
+
+  runApp(CapturadorApp(graphQLClient: client));
 }
 
 class CapturadorApp extends StatelessWidget {
-  const CapturadorApp({super.key});
+  final ValueNotifier<GraphQLClient> graphQLClient;
+
+  const CapturadorApp({super.key, required this.graphQLClient});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Terrasacha - Captura de Datos',
-      debugShowCheckedModeBanner: false,
-      theme: terrasachaTheme,
-      initialRoute: '/proyectos',
-      routes: {
-        '/proyectos': (context) => const ProyectosMenuScreen(),
-        '/predios': (context) => const PrediosMenuScreen(),
-        '/parcelas': (context) => const ParcelasMenuScreen(),
-        '/captura': (context) => const CapturaDatosScreen(),
-        '/sincronizacion': (context) => const SincronizacionScreen(),
-        '/revision': (context) => const RevisionScreen(),
-        '/registros': (context) => const RegistrosGuardadosScreen(),
-      },
+    return GraphQLProvider(
+      client: graphQLClient,
+      child: MaterialApp(
+        title: 'Terrasacha - Captura de Datos',
+        debugShowCheckedModeBanner: false,
+        theme: terrasachaTheme,
+        initialRoute: '/proyectos',
+        routes: {
+          '/proyectos': (context) => const ProyectosMenuScreen(),
+          '/predios': (context) => const PrediosMenuScreen(),
+          '/parcelas': (context) => const ParcelasMenuScreen(),
+          '/captura': (context) => const CapturaDatosScreen(),
+          '/sincronizacion': (context) => const SincronizacionScreen(),
+          '/revision': (context) => const RevisionScreen(),
+          '/registros': (context) => const RegistrosGuardadosScreen(),
+        },
+      ),
     );
   }
 }
